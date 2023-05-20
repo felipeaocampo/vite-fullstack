1066e952f792205a8c0c18ff4162b2acd628b3be
Felipe Ocampo <felipe.aocampo@gmail.com>
2023-05-20 14:41:52 -0700

    frontend and backend connected

--- vite-fullstack/src/App.tsx
+++ vite-fullstack/src/App.tsx
@@ -1,6 +1,19 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  // const [data, setData] = useState<any>(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      const res = await fetch(`http://localhost:3000/`);
+      const data = await res.json();
+      // setData(data);
+      console.log(data);
+    };
+    getData();
+  }, []);
+
   return (
     <>
       <h1>Heloo</h1>
--- vite-fullstack/src/server.ts
+++ vite-fullstack/src/server.ts
@@ -1,10 +1,19 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
 
+//GENERAL MIDDLEWARES
+app.use(cors());
+
+app.use(`*`, (req, _res, next) => {
+  console.log(req.url, `this is the req.url`);
+  next();
+});
+
 app.get(`/`, (_req, res) => {
-  res.status(200).send(`All good to go!`);
+  res.status(200).json({ hello: `World!` });
 });
 
 app.listen(port, () => {
